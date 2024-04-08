eacf317d18d8b34c70cce0ce9e935ee5e5ca1e69
JanainaSantosSilva <janaina.santos231@hotmail.com>
2024-04-08 11:47:14 -0300

    Novos implementos

--- dist/VueTheMask.umd.js
+++ dist/VueTheMask.umd.js
@@ -391,7 +391,7 @@ function directive_event(name) {
   }
 });
 // CONCATENATED MODULE: ./node_modules/vue-loader/lib/template-compiler?{"id":"data-v-1d380615","hasScoped":false,"optionsId":"1","buble":{"transforms":{}}}!./node_modules/vue-loader/lib/selector.js?type=template&index=0!./src/component.vue
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('input',{directives:[{name:"mask",rawName:"v-mask",value:(_vm.config),expression:"config"}],attrs:{"type":"text"},domProps:{"value":_vm.display},on:{"input":_vm.onInput}})}
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_h;return _c('input',{directives:[{name:"mask",rawName:"v-mask",modelValue:(_vm.config),onUpdateModelValue:(value) => { _vm.config = value;}, expression:"config"}],attrs:{"type":"text"},domProps:{"value":_vm.display},on:{"input":_vm.onInput}})}
 var staticRenderFns = []
 
 // CONCATENATED MODULE: ./node_modules/vue-loader/lib/runtime/component-normalizer.js
